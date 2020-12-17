ce297f627ea8ba98256fe5e3ea9b796daef1b6f7
Lisa <bardelli.lisa@gmail.com>
2020-12-17 19:27:51 +0000

    Add outcome of pairing session with Chris

--- spec/features/updating_bookmarks_spec.rb
+++ spec/features/updating_bookmarks_spec.rb
@@ -5,16 +5,8 @@ feature 'When I want to be able to update an existing bookmark' do
     click_button("Update")
     fill_in("title", with: 'Test')
     click_button("Find")
-    #expect(page).to have_content('Test')
-    #expect(page).to have_content('www.test.com')
     expect(page).to have_button("Update")
 
   end
 
-  # scenario 'I want to update my found bookmark' do
-  #   Bookmark.create("Test", "www.test.com")
-  #   visit('/bookmarks')
-  #   click_button("Update")
-  #   fill_in
-  # end
 end
--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -2,7 +2,5 @@ require 'pg'
 
 def setup_test_database
   connection = PG.connect(dbname: 'bookmark_manager_test')
-
-  # Clear the bookmarks table
   connection.exec("TRUNCATE bookmarks;")
 end
